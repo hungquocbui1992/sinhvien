fbbb48330a606f573efe07575b60015cda85e8f6
hungquocbui1992 <buiquochung623@gmail.com>
2022-03-08 21:38:28 +0700

    nop bai hiển thị tên các sinh viên
    ok

--- src/index.js
+++ src/index.js
@@ -15,3 +15,82 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+const students = [
+  {
+    company: 'Alfreds Futterkiste',
+    contact: 'Maria Anders',
+    country: 'Germany'
+  },
+  {
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico'
+  },
+  {
+    company: 'Ernst Handel',
+    contact: 'Roland Mendel',
+    country: 'Austria'
+  },
+  {
+    company: 'Island Trading',
+    contact: 'Helen Bennett',
+    country: 'UK'
+  },
+  {
+    company: 'Laughing Bacchus Winecellars',
+    contact: 'Yoshi Tannamuri',
+    country: 'Canada'
+  },
+  {
+    company: 'Magazzini Alimentari Riuniti',
+    contact: 'Giovanni Rovelli',
+    country: 'Italy'
+  }
+]
+const element = <div>
+<h2>Students</h2>
+<table>
+  <thead>
+    <tr>
+      <th>Company</th>
+      <th>Contact</th>
+      <th>Country</th>
+    </tr>
+  </thead>
+  <tbody>
+    { students.map((student) => (
+      <tr>
+        <td>{student.company}</td>
+        <td>{student.contact}</td>
+        <td>{student.country}</td>
+      </tr>
+    )) }
+  </tbody>
+</table>
+</div>
+ReactDOM.render(
+  <div>
+    <h2>Students</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+      </thead>
+      <tbody>
+        { students.map((student) => (
+          <tr>
+            <td>{student.company}</td>
+            <td>{student.contact}</td>
+            <td>{student.country}</td>
+          </tr>
+        )) }
+      </tbody>
+    </table>
+  </div>,
+  document.getElementById('root')
+);
+
+
